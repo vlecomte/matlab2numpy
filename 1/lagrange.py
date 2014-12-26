208d8fedeeeba12ab9e15c889c0d18826dd5217f
Victor Lecomte <victor.lecomte@student.uclouvain.be>
2014-12-26 21:22:26 +0100

    Cleaning up and commenting

--- 1/lagrange.py
+++ 1/lagrange.py
@@ -1,8 +1,12 @@
 from numpy import *
 import warnings
 
+# ========================
+#  Lagrange interpolation
+# ========================
+
 def lagrange_naive(x,X,U):
-    # Not pre-allocating is not possible with numpy
+    # Not pre-allocating is not possible with numpy, as far as I know.
     uh = zeros(shape(x))
     n = len(X)
     
@@ -23,18 +27,23 @@ def lagrange_clever(x,X,U):
     for i in range(n):
         for j in range(n):
             if j != i:
+                # Assigning the i-th line of phi.
                 phi[i] = phi[i] * (x-X[j]) / (X[i]-X[j])
     
+    # Matrix multiplication is done with dot(), not with '*'.
     return dot(U, phi)
 
 def lagrange_super(x,X,U):
-    
-    X2,x2 = meshgrid(X,x)
-    diff_xX = x2-X2
+    # X_m1 means X replicated m times in dimension 0 and 1 time in dimension 1,
+    # and so forth.
+    X_m1, x_1n = meshgrid(X,x)
+    diff_xX = x_1n - X_m1
     prod_xX = prod(diff_xX,1)
     
-    X3,X4 = meshgrid(X,X)
-    diff_XX = X4 - X3 + eye(X.size)
+    X_n1, X_1n = meshgrid(X,X)
+    # We replace the zeros on the diagonal with ones so we can take the product
+    # of the lines directly.
+    diff_XX = X_1n - X_n1 + eye(len(X))
     coeffs = U / prod(diff_XX,1)
     
     sum_prod = dot(1/diff_xX, coeffs)
@@ -42,6 +51,26 @@ def lagrange_super(x,X,U):
     return prod_xX * sum_prod
 
 def lagrange_polyfit(x,X,U):
-    warnings.simplefilter('ignore', RankWarning)
+    # Those functions are used the same way as in MATLAB.
     p = polyfit(X,U,len(X))
     return polyval(p,x)
+
+# ===================
+#  Utility functions
+# ===================
+
+def remove_near(x,X, eps=spacing(1)):
+    x_n1, X_1m = meshgrid(x,X)
+    # We only take x where it is farther than eps from all elements of X.
+    return x[abs(x_n1-X_1m).min(0) > eps]
+
+def call_all():
+    # Calling all the functions so that they are already loaded in memory when
+    # we benchmark them.
+    x = array([])
+    X = array([0,1])
+    U = array([0,1])
+    lagrange_naive(x,X,U)
+    lagrange_clever(x,X,U)
+    lagrange_super(x,X,U)
+    lagrange_polyfit(x,X,U)
--- 1/test1.py
+++ 1/test1.py
@@ -13,11 +13,9 @@ X = linspace(0,5,n+1)
 # Operators on arrays are element-by-element operators by default.
 U = 1 / (X+1)
 
-# This is approximately the minimum value to avoid division by zero. The
-# behaviour is not exactly well defined, so it's not just spacing(1).
-eps = spacing(10)
-x = linspace(eps,5-eps,m)
-uh = lagrange_polyfit(x,X,U)
+# We remove sigular points (points near X).
+x = remove_near(linspace(0,5,m), X)
+uh = lagrange_super(x,X,U)
 
 # The plot function works pretty much like its MATLAB equivalent. However, it
 # blocks the execution until the window is closed.
@@ -31,24 +29,34 @@ show()
 print('Efficiency tests with m={}'.format(m))
 
 nSet = [2,5,10,20,50,100]
-na = zeros(shape(nSet))
-cl = zeros(shape(nSet))
-su = zeros(shape(nSet))
-po = zeros(shape(nSet))
+# This is basically laziness, but list comprehensions are cool.
+[na,cl,su,po] = [zeros(shape(nSet)) for i in range(4)]
+
+# Will ignore warnings from polyfit. In practice it is a bad idea to use higher
+# order polynomial interpolations. And possibly even worse to ignore warnings.
+warnings.simplefilter('ignore', RankWarning)
+# Calls all functions so that loading the function into memory will not have an
+# influence on the running time.
+call_all()
 
 for i,n in enumerate(nSet):
     X = linspace(0,10,n+1)
     print('  Order of interpolation is {}'.format(n))
     
-    # Function wrapper to measure execution time
+    # Function wrapper to measure execution time. It copies function arguments
+    # and hands them to the given function.
     def tictoc(f, *args):
         start = clock()
         f(*args)
         end = clock()
         return end-start
     
-    x = linspace(eps,10-eps,m)
+    # Again, we remove singular points.
+    x = remove_near(linspace(0,10,m), X)
     U = 1 / (X+1)
+    
+    # Benchmarking every version. The naive version is too slow to be run above
+    # degree 20.
     if n <= 20:
         na[i] = tictoc(lagrange_naive, x,X,U)
         print('    Naive lagrange:   {:.3f}'.format(na[i]))
@@ -59,6 +67,8 @@ for i,n in enumerate(nSet):
     po[i] = tictoc(lagrange_polyfit, x,X,U)
     print('    Polyfit lagrange: {:.3f}'.format(po[i]))
 
+# A log-log plot will be more readable, and complexities of the form O(C*n^k)
+# will appear as lines.
 loglog(nSet,na,'.-r', nSet,cl,'.-b', nSet,su,'.-g', nSet,po,'.-k')
 xlabel('Order of interpolation')
 ylabel('CPU time (s)')
